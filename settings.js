a2576d7ff73eb05b886f20f4c5da93c94ce3ffec
humanga <114870515+humanga@users.noreply.github.com>
2022-10-09 12:07:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,14 +21,14 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
+global.owner = ['94766241808']
+global.premium = ['94766241808']
+global.ownernomer = '94766241808'
+global.ownername = 'Ushira Paranga'
 global.botname = 'Cheems Bot MD V2'
 global.footer = 'Xeon Bot Inc.'
 global.ig = 'https://github.com/DGXeon'
-global.region = 'India, NorthEast, Mizoram'
+global.region = 'Srilanka,Galle,Asia'
 global.sc = 'https://github.com/DGXeon/CheemsBot-MD2'
 global.myweb = 'https://youtu.be/imFIX-Wrt3s'
 global.packname = 'Cheems Bot'
